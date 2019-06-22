490db3cd2e0c291e19335e1c4317c1a01eef2a31
konrumi <konrumi@gmail.com>
2019-06-22 20:17:59 +0800

    :sparkles: add login redirect

--- src/utils/responsePreprocessing.js
+++ src/utils/responsePreprocessing.js
@@ -4,15 +4,36 @@ const responsePreprocessing = function (res, silent = false, errorMap = {}) {
   return new Promise((resolve, reject) => {
     let resData = res.data
     if (resData.errno === 0) {
+      // response ok
       resolve(resData.data)
     } else {
+      // response with error
       let message = errorMap[resData.errno] || resData.errmsg
 
-      if (!silent) {
-        Modal.error({
-          title: '错误',
-          content: message || '发生错误…'
-        })
+      switch (resData.errno) {
+        case 1001 :
+          // not login
+          Modal.error({
+            title: '错误',
+            content: '未登录，3秒后跳转至登录页面…',
+            okButtonProps: { style: { display: 'none' } },
+            cancelButtonProps: { style: { display: 'none' } }
+          })
+
+          setTimeout(() => {
+            window.location.href = '/login/'
+          }, 3000)
+          break
+
+        default :
+          // default error
+          if (!silent) {
+            Modal.error({
+              title: '错误',
+              content: message || '发生错误…'
+            })
+          }
+          break
       }
 
       reject({ message })
